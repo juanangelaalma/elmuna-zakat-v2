e71b6d9f3df1ca13d909c3f04ae727e31bf37a91
Juan Alma <juanangelaalma@gmail.com>
2025-12-11 21:31:57 +0700

    Re-layouting app-side bar

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -89,23 +89,13 @@ const mainNavItems: NavItem[] = [
             },
         ]
     },
-    {
-        title: 'Default Value',
-        href: defaultValue(),
-        icon: Settings,
-    },
 ];
 
 const footerNavItems: NavItem[] = [
     {
-        title: 'Repository',
-        href: 'https://github.com/laravel/react-starter-kit',
-        icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#react',
-        icon: BookOpen,
+        title: 'Default Value',
+        href: defaultValue(),
+        icon: Settings,
     },
 ];
 
--- resources/js/components/nav-footer.tsx
+++ resources/js/components/nav-footer.tsx
@@ -9,6 +9,7 @@ import {
 import { resolveUrl } from '@/lib/utils';
 import { type NavItem } from '@/types';
 import { type ComponentPropsWithoutRef } from 'react';
+import { Link, usePage } from '@inertiajs/react';
 
 export function NavFooter({
     items,
@@ -30,10 +31,8 @@ export function NavFooter({
                                 asChild
                                 className="text-neutral-600 hover:text-neutral-800 dark:text-neutral-300 dark:hover:text-neutral-100"
                             >
-                                <a
-                                    href={resolveUrl(item.href)}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
+                                <Link
+                                    href={item.href}
                                 >
                                     {item.icon && (
                                         <Icon
@@ -42,7 +41,7 @@ export function NavFooter({
                                         />
                                     )}
                                     <span>{item.title}</span>
-                                </a>
+                                </Link>
                             </SidebarMenuButton>
                         </SidebarMenuItem>
                     ))}
