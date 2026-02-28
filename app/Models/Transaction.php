dd66aa5eacf2326225cfd164f89b644c24a50a9a
Juan Alma <juanangelaalma@gmail.com>
2026-02-28 13:12:15 +0700

    [Feat] Dashboard zakat live

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -10,4 +10,9 @@ class Transaction extends Model
     use SoftDeletes;
 
     protected $guarded = ["id"];
+
+    public function details()
+    {
+        return $this->hasMany(TransactionDetail::class);
+    }
 }
